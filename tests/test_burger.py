03986b6553474cfa0cf9d5154920f3b6766f8449
Nast8065 <n9069968806n@yandex.ru>
2025-10-23 22:51:05 +0700

    Update test_burger.py
    Убрала фикстуры из тестов

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -3,50 +3,6 @@ from unittest.mock import Mock
 from tests import data
 
 
-@pytest.fixture
-def mock_bun():
-    return data.get_mock_bun()
-
-
-@pytest.fixture
-def mock_ingredient_sauce():
-    return data.get_mock_ingredient_sauce()
-
-
-@pytest.fixture
-def mock_ingredient_filling():
-    return data.get_mock_ingredient_filling()
-
-
-@pytest.fixture
-def mock_ingredient_cheese():
-    return data.get_mock_ingredient_cheese()
-
-
-@pytest.fixture
-def empty_burger():
-    from praktikum.burger import Burger
-    return Burger()
-
-
-@pytest.fixture
-def burger_with_bun(mock_bun):
-    from praktikum.burger import Burger
-    burger = Burger()
-    burger.set_buns(mock_bun)
-    return burger
-
-
-@pytest.fixture
-def burger_with_ingredients(mock_bun, mock_ingredient_sauce, mock_ingredient_filling):
-    from praktikum.burger import Burger
-    burger = Burger()
-    burger.set_buns(mock_bun)
-    burger.add_ingredient(mock_ingredient_sauce)
-    burger.add_ingredient(mock_ingredient_filling)
-    return burger
-
-
 def test_burger_price(burger_with_ingredients, mock_bun, mock_ingredient_sauce, mock_ingredient_filling):
     expected_price = (mock_bun.get_price() * 2 +
                      mock_ingredient_sauce.get_price() +
@@ -110,3 +66,4 @@ def test_burger_with_only_bun_receipt(burger_with_bun, mock_bun):
 
     expected_receipt = "\n".join(expected_receipt_lines)
     assert receipt == expected_receipt
+
