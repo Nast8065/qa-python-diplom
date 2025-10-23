f0f4bca15527b769d76ee0e5409a676ec8ab0fe4
Nast8065 <n9069968806n@yandex.ru>
2025-10-23 23:45:31 +0700

    Update test_main_functionality.py
    разбила проверки на атомарные

--- tests/test_main_functionality.py
+++ tests/test_main_functionality.py
@@ -7,10 +7,20 @@ class TestMainFunctionality:
 
     @allure.title("TC-01: Переход по клику на 'Конструктор'")
     @allure.description("Тест проверяет навигацию между конструктором и лентой заказов")
-    def test_constructor_navigation(self, main_page):
+    def test_constructor_navigation(self, browser):
+        from pages.main_page import MainPage
+        from pages.order_feed_page import OrderFeedPage
+        
+        main_page = MainPage(browser)
+        order_feed_page = OrderFeedPage(browser)
+        
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+            main_page.wait_for_main_page_loaded()
+
         with allure.step("Перейти в ленту заказов"):
             main_page.click_order_feed()
-            assert "feed" in main_page.get_current_url()
+            assert "feed" in order_feed_page.get_current_url()
 
         with allure.step("Вернуться в конструктор через навигацию"):
             main_page.click_constructor()
@@ -19,17 +29,35 @@ class TestMainFunctionality:
 
     @allure.title("TC-02: Переход по клику на 'Лента Заказов'")
     @allure.description("Тест проверяет переход из конструктора в ленту заказов")
-    def test_order_feed_navigation(self, main_page):
+    def test_order_feed_navigation(self, browser):
+        from pages.main_page import MainPage
+        from pages.order_feed_page import OrderFeedPage
+        
+        main_page = MainPage(browser)
+        order_feed_page = OrderFeedPage(browser)
+        
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+            main_page.wait_for_main_page_loaded()
+
         with allure.step("Кликнуть на 'Лента Заказов'"):
             main_page.click_order_feed()
 
         with allure.step("Проверить переход на страницу ленты заказов"):
-            assert "feed" in main_page.get_current_url()
-            assert main_page.is_order_feed_active()
+            assert "feed" in order_feed_page.get_current_url()
+            assert order_feed_page.is_order_feed_active()
 
     @allure.title("TC-03: Открытие модального окна с деталями ингредиента")
     @allure.description("Тест проверяет открытие модального окна при клике на ингредиент")
-    def test_ingredient_modal_opening(self, main_page):
+    def test_ingredient_modal_opening(self, browser):
+        from pages.main_page import MainPage
+        
+        main_page = MainPage(browser)
+        
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+            main_page.wait_for_main_page_loaded()
+
         with allure.step("Кликнуть на первый ингредиент"):
             main_page.click_ingredient(0)
 
@@ -38,7 +66,17 @@ class TestMainFunctionality:
 
     @allure.title("TC-04: Закрытие модального окна ингредиента")
     @allure.description("Тест проверяет закрытие модального окна кликом по крестику")
-    def test_ingredient_modal_closing(self, main_page, order_modal):
+    def test_ingredient_modal_closing(self, browser):
+        from pages.main_page import MainPage
+        from pages.order_modal import OrderModal
+        
+        main_page = MainPage(browser)
+        order_modal = OrderModal(browser)
+        
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+            main_page.wait_for_main_page_loaded()
+
         with allure.step("Открыть модальное окно ингредиента"):
             main_page.click_ingredient(0)
             assert main_page.is_ingredient_modal_visible()
@@ -51,20 +89,28 @@ class TestMainFunctionality:
 
     @allure.title("TC-05: Увеличение счетчика ингредиента при добавлении")
     @allure.description("Тест проверяет увеличение счетчика ингредиента при добавлении в заказ")
-    def test_ingredient_counter_increase(self, main_page, authenticated_user):
+    def test_ingredient_counter_increase(self, authenticated_user):
         with allure.step("Получить начальное значение счетчика ингредиента"):
-            initial_counter = main_page.get_ingredient_counter(0)
+            initial_counter = authenticated_user.get_ingredient_counter(0)
 
         with allure.step("Добавить ингредиент в конструктор"):
-            main_page.add_ingredient_to_constructor(0)
+            authenticated_user.add_ingredient_to_constructor(0)
 
         with allure.step("Проверить увеличение счетчика"):
-            new_counter = main_page.get_ingredient_counter(0)
-            assert new_counter != initial_counter, "Счетчик ингредиента должен увеличиться"
+            new_counter = authenticated_user.get_ingredient_counter(0)
+            assert new_counter > initial_counter, f"Счетчик ингредиента должен увеличиться. Было: {initial_counter}, стало: {new_counter}"
 
     @allure.title("TC-06: Навигация по разделам конструктора")
     @allure.description("Тест проверяет переключение между разделами конструктора")
-    def test_constructor_sections_navigation(self, main_page):
+    def test_constructor_sections_navigation(self, browser):
+        from pages.main_page import MainPage
+        
+        main_page = MainPage(browser)
+        
+        with allure.step("Открыть главную страницу"):
+            main_page.open()
+            main_page.wait_for_main_page_loaded()
+
         with allure.step("Проверить активность раздела 'Булки' по умолчанию"):
             assert main_page.is_buns_section_active()
 
